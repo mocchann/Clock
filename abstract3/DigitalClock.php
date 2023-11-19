2976741bbb71a6d8c8e8b6f9d170ee87b51eb39a
もっちゃん <s7056i3056@gmail.com>
2023-11-19 23:40:14 +0900

    feat: メソッドをオーバーライドする実装

---
+++ abstract3/DigitalClock.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types = 1);
+
+require_once dirname(__FILE__) . '/Clock.php';
+
+class DigitalClock extends Clock
+{
+    private $color;
+
+    public function setTime(int $time): void
+    {
+        if (date('H', $time) >= 6 && date('H', $time) <= 21) {
+            $this->color = 'white';
+        } else {
+            $this->color = 'blue';
+        }
+        $this->time = $time;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function show(): string
+    {
+        return date('H:i', $this->time);
+    }
+}
